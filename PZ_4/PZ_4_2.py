73f6d9b975004082062980b9e508c0974fd22ed2
auseklis-s <sogoyn.p@gmail.com[student@A420-09 ]$ git config --global user.name>
2023-11-08 18:57:38 +0300

    task 4

--- PZ_4/PZ_4_2.py
+++ PZ_4/PZ_4_2.py
@@ -1,24 +1,33 @@
 """Дано целое число N (>0). С помощью операций деления нацело и взятия остатка от деления определить,
  имеются ли в записи числа N нечетные цифры. Если имеются, то вывести TRUE, если нет - вывести FALSE."""
 
-def second_task():
 
+def second_task():
+    list = []
+    end = False
     try:
         number = int(input('Введите любое целое число N, которое больше 0: '))
         if number > 0:
             div = 10
             first = 1
             ost = number % 10
-            print(ost)
+            list.append(ost)
             while first != 0:
                 first = number // div
                 div = div * 10
                 ost = first % 10
-                print(first, ost)
+                list.append(ost)
+            for i in list:
+                if i % 2 != 0:
+                    end = True
+                    print(end)
+                    exit()
+            print(end)
+
         else:
             print('Введены некорректные данные')
             second_task()
-    except:
+    except ValueError:
         print('Введены некорректные данные')
         second_task()
 
