397f511d9c5bcbc86ce8e58b7b7401b6ce747f80
posovestii <kimyana2007@gmail.com>
2023-11-22 19:06:01 +0300

    task 5

---
+++ PZ_5/PZ_5_1.py
@@ -0,0 +1,17 @@
+# Составить функцию, которая напечатает 40 любых символов
+from random import randint
+
+symbols_list = ['a', 'b','c','d','e', 'f','j']
+def any40s():
+    try:
+        word = len(symbols_list) - 1
+        for i in range(40):
+            a = randint(0, word)
+            res = symbols_list[a]
+            print(res)
+    except:
+        print("Произошла ошибка!")
+        any40s()
+
+
+any40s()
