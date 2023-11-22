6f51d3aac8f52640dcde853ea87c2084b99c60fc
posovestii <kimyana2007@gmail.com>
2023-11-22 19:26:32 +0300

    task 5

--- PZ_5/PZ_5_1.py
+++ PZ_5/PZ_5_1.py
@@ -5,6 +5,7 @@ symbols_list = ['a', 'b','c','d','e', 'f','j']
 def any40s():
     try:
         word = len(symbols_list) - 1
+        print('Ваши 40 любых символов: ')
         for i in range(40):
             a = randint(0, word)
             res = symbols_list[a]
