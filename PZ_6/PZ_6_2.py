91d984354b9ddbff01dc264964438d4a49ab9736
posovestii <kimyana2007@gmail.com>
2023-11-28 17:14:36 +0300

    task 6

--- PZ_6/PZ_6_2.py
+++ PZ_6/PZ_6_2.py
@@ -1,15 +1,22 @@
 """
-Дан список размера N. Найти номера тех элементов список, которые больше своего
+Дан список размера N. Найти номера тех элементов списка, которые больше своего
 левого соседа, и количество таких элементов. Найденные номера выводить в
 порядке их убывания.
 """
 
-lst = [12,24,123,6,178,3,63,1234,1,23,4,2]
-"""
-counter = 0
-for numbers[counter] in numbers:
-    counter +=1
- #   print(numbers[counter] , numbers[counter-1])
-    if numbers[counter] > numbers[counter-1]:
-        print(numbers.index(numbers[counter]))
-        """
+numbers = [12,24,123,6,178,3,63,1234,1,23,4,2]
+def task6():
+    try:
+        number = 0
+        nums_rev = []
+        for numbers[number] in numbers:
+            number +=1
+         #   print(numbers[counter] , numbers[counter-1])
+            if numbers[number] > numbers[number-1]:
+                nums_rev.append(numbers.index(numbers[number]))
+
+    except:
+        print('Номера чисел: ', nums_rev[::-1] ,'\nКоличество элементов, соответсвующее условиям: ', len(nums_rev))
+
+
+task6()
