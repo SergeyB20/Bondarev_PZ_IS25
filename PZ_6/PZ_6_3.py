7f9eeda2e98ae85f9fc97161b8577c90cb436045
posovestii <kimyana2007@gmail.com>
2023-11-28 17:37:19 +0300

    task 6

--- PZ_6/PZ_6_3.py
+++ PZ_6/PZ_6_3.py
@@ -6,14 +6,17 @@
 
 numbers = [12,1234,123,6,178,3,63,1,23,4,2]
 
-max_num, min_num = numbers.index(max(numbers)), numbers.index(min(numbers))
+def zero():
+    max_num, min_num = numbers.index(max(numbers)), numbers.index(min(numbers))
 
-if max_num>min_num :
-    for i in range(min_num+1,max_num):
-        numbers[i]=0
-if max_num<min_num :
-    for i in range(max_num+1,min_num):
-        numbers[i]=0
+    if max_num>min_num :
+        for i in range(min_num+1,max_num):
+            numbers[i]=0
+    if max_num<min_num :
+        for i in range(max_num+1,min_num):
+            numbers[i]=0
 
 
-print(numbers)
+    print('Список с обнулением чисел между максимальным и минимальным значением: ',numbers)
+
+zero()
