547c6a9276f5223ecaf3ed60446a2ee56e77a1da
Sergey <125741718+SergeyB20@users.noreply.github.com>
2024-02-21 13:38:23 +0300

    12_1

--- PZ_12/PZ_12_1.py
+++ PZ_12/PZ_12_1.py
@@ -3,19 +3,16 @@
 первая половина элементов А,в С – вторая половина элементов А. Найти произведение
 соответствующих элементов последовательностей В и С. Найти среднее арифметической
 полученной последовательности."""
-from functools import reduce
+
 import random
 colwo = int(input('Введите сколько будет чисел в последовательности(Четное количество): '))
 if colwo % 2 == 0:
     list_ = [random.randint(0, 20) for i in range(colwo)]
     print(list_)
-    listB,listC = [], []
-    for i in list_:
-        [listB.append(i) if list_.index(i) < ((len(list_)//2)-1) else listC.append(i)]
+    listB = list_[:len(list_)//2]
+    listC = list_[len(list_)//2:]
     print(listB,listC)
     A = [x * y for x, y in zip(listB, listC)]
     B = sum(A)/len(A)
     print('Произведение соответствующих элементов последовательностей', A)
     print('Среднее арифметическое полученной последовательности', B)
-
-
