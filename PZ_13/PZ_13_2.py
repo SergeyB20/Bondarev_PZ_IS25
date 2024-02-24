3b2cff77e49eea312e68c89ce3a61625606e661f
posovestii <kimyana2007@gmail.com>
2024-02-24 10:33:59 +0300

    13.2

--- PZ_13/PZ_13_2.py
+++ PZ_13/PZ_13_2.py
@@ -1,12 +1,14 @@
 # В матрице найти сумму отрицательных элементов в первой трети матрицы
 
 import random
-matrix = [[random.randint(-20,20) for i in range(3)] for i in range(3)]
+matrix = [[random.randint(-20,20) for i in range(6)] for i in range(6)]
 a = 0
 def cogl(towns):
     for town in towns:
-            if town < 0:
-                yield town
+        for i in town:
+            if i < 0:
+                yield i
 
-print('Отрицательные числа: ',(list(cogl(matrix[0]))))
-print('Сумма отрицательных чисел: ',sum(list(cogl(matrix[0]))))
+print(matrix[:(len(matrix)//3)])
+
+print('Сумма отрицательных чисел: ',sum(list(cogl(matrix[:(len(matrix)//3)]))))
