988bcc088aacafeafb0591ecb8f54d1ff149de0f
posovestii <kimyana2007@gmail.com>
2024-05-27 18:49:47 +0300

    17

--- PZ_17/PZ_17_1.py
+++ PZ_17/PZ_17_1.py
@@ -3,38 +3,44 @@ import tkinter as tk
 root = tk.Tk()
 root.title("Персональная информация")
 
+text_info = tk.Label(root, text='Форма заявки на работу в зоопарке')
+text_info.config(font=("Courier", 17))
+text_info.grid(row=0,column=0)
+text_sub_info = tk.Label(root, text='Пожалуйста, заполните форму. Обязательные поля помечены *')
+text_sub_info.config(font=("Courier", 8))
+text_sub_info.grid(row=1,column=0)
 personal_frame = tk.LabelFrame(root, text="Персональная информация")
-personal_frame.grid(row=0, column=0, padx=5, pady=5, sticky="nsew")
+personal_frame.grid(row=2, column=0, padx=7, pady=7, sticky="nsew")
 contact_frame = tk.LabelFrame(root, text="Контактная информация")
-contact_frame.grid(row=1, column=0, padx=5, pady=5, sticky="nsew")
+contact_frame.grid(row=3, column=0, padx=7, pady=7, sticky="nsew")
 preferences_frame = tk.LabelFrame(root, text="Выберите ваших любимых животных:")
-preferences_frame.grid(row=2, column=0, padx=5, pady=5, sticky="nsew")
+preferences_frame.grid(row=4, column=0, padx=7, pady=7, sticky="nsew")
 
 
-name_label = tk.Label(personal_frame, text="Имя:")
-name_label.grid(row=0, column=0)
+name_label = tk.Label(personal_frame, text="Имя *")
+name_label.grid(row=0, column=0, padx=7, pady=7)
 name_entry = tk.Entry(personal_frame)
-name_entry.grid(row=0, column=1)
+name_entry.grid(row=0, column=1, padx=7, pady=7)
 
-age_label = tk.Label(personal_frame, text="Телефон:")
-age_label.grid(row=1, column=0)
+age_label = tk.Label(personal_frame, text="Телефон")
+age_label.grid(row=1, column=0, padx=7, pady=7)
 age_entry = tk.Entry(personal_frame)
-age_entry.grid(row=1, column=1)
+age_entry.grid(row=1, column=1, padx=7, pady=7)
 
-email_label = tk.Label(personal_frame, text="Email:")
-email_label.grid(row=2, column=0)
+email_label = tk.Label(personal_frame, text="Email *")
+email_label.grid(row=2, column=0, padx=7, pady=7)
 email_entry = tk.Entry(personal_frame)
-email_entry.grid(row=2, column=1)
+email_entry.grid(row=2, column=1, padx=7, pady=7)
 
 
 
-phone_label = tk.Label(contact_frame, text="Возврат:")
-phone_label.grid(row=0, column=0, sticky="w")
+phone_label = tk.Label(contact_frame, text="Возврат *")
+phone_label.grid(row=0, column=0, padx=7, pady=7, sticky="w")
 phone_entry = tk.Entry(contact_frame)
-phone_entry.grid(row=0, column=1, sticky="w")
+phone_entry.grid(row=0, column=1, padx=7, pady=7, sticky="w")
 
-gender_label = tk.Label(contact_frame, text="Пол:")
-gender_label.grid(row=1, column=0, sticky="w")
+gender_label = tk.Label(contact_frame, text="Пол")
+gender_label.grid(row=1, column=0, padx=7, pady=7, sticky="w")
 
 gender_options = "Мужской"
 gender_options1 = "Женский"
@@ -42,10 +48,10 @@ gender_var = tk.StringVar()
 gender_menu = tk.OptionMenu(contact_frame, gender_var, gender_options, gender_options1)
 gender_menu.grid(row=1, column=1)
 
-phone_label1 = tk.Label(contact_frame, text="Перечислите личные качества:")
-phone_label1.grid(row=2, column=0, sticky="w")
+phone_label1 = tk.Label(contact_frame, text="Перечислите личные качества")
+phone_label1.grid(row=2, column=0, padx=7, pady=7, sticky="w")
 phone_entry = tk.Text(contact_frame, height=4, width=25)
-phone_entry.grid(row=3, column=0)
+phone_entry.grid(row=3, column=0, padx=7, pady=7)
 
 animals_frame = tk.Frame(preferences_frame)
 animals_frame.grid(row=0, column=0)
---
+++ PZ_17/PZ_17_3.py
@@ -0,0 +1,33 @@
+import os
+# task 1
+# print(os.listdir('../PZ_11'))
+# task 2
+try:
+    os.mkdir('../test')
+    os.mkdir('../test/test1')
+except:
+    pass
+
+# task 3
+# os.rename('../PZ_6/1.txt', '../test/1.txt')
+# os.rename('../PZ_6/2.txt', '../test/2.txt')
+# os.rename('../PZ_7/3.txt', '../test/test1/4.txt')
+
+# task 4
+# path = os.listdir('../test')
+# for i in path:
+#     print(os.path.getsize(f'../test/{i}'))
+
+# task 5
+# path = os.listdir('../PZ_11')
+# path = min(path, key=len)
+#
+# print(os.path.basename(f'../PZ_11/{path}'))
+
+# task 6
+
+os.startfile('123.txt.txt', 'edit')
+
+# task 7
+open('123.txt', 'w')
+# os.remove('123.txt')
